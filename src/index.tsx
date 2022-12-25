9aa124ab25b3aa20b178bedb9b62b386915edac7
daria <pojidaeva.daria@gmail.com>
2022-12-25 16:56:33 +0200

    add index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+//const title = React.createElement('h1', null, 'Hello')
+const title = <h1 id="title">Hello</h1>
+console.log(title)
+
+const content = (
+  <div>
+    <ul>
+      <li>List item 1</li>
+      <li>List item 2</li>
+    </ul>
+    <p>Hello world</p>
+  </div>
+)
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>{title}</React.StrictMode>
 );
 
