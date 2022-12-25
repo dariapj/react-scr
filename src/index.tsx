47e00d467771946ac283db0d20164c4480a5bab8
daria <pojidaeva.daria@gmail.com>
2022-12-25 17:05:13 +0200

    add index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -15,13 +15,16 @@ import ReactDOM from 'react-dom/client';
   </div>
 )*/
 
-function App() {
+const App = () => {
   return (
     <div>
       <h1>Hello React</h1>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit autem quos inventore sunt voluptates ea enim odit minus fugit ullam itaque quis, dolore veritatis sint, quam quo aut? Velit, a!
       </p>
+      <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit autem quos inventore sunt voluptates ea enim odit minus fugit ullam itaque quis, dolore veritatis sint, quam quo aut? Velit, a!
+    </p>
     </div>
   )
 }
