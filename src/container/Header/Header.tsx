9206a1e89f3e96d660e62325b2929c82a7ffa31e
daria <pojidaeva.daria@gmail.com>
2022-12-26 18:40:51 +0200

    add logo

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,11 +1,12 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container'
 import Menu from 'components/Menu/Menu';
+import CartHeader from 'components/CartHeader/CartHeader';
+import Logo from 'components/Logo/Logo'
 
 type Props = {}
 const Header = (props: Props) => {
@@ -21,8 +22,10 @@ const Header = (props: Props) => {
             aria-label="menu"
             sx={{ mr: 2 }}>
                 <MenuIcon />
-                </IconButton><Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>News</Typography>
+                </IconButton>
+                <Logo />
                 <Menu/>
+                <CartHeader/>
                 </Toolbar>
                 </Container>
                 </AppBar>
