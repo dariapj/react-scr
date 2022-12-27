b34082e151d6c2c5d3780346fd957c4d76880421
daria <pojidaeva.daria@gmail.com>
2022-12-27 18:50:43 +0200

    add products

---
+++ src/components/Products/ProductsList.tsx
@@ -0,0 +1,26 @@
+import {Grid} from '@mui/material'
+import ProductsListItem from './ProductsListItem'
+
+type Props = {}
+const ProductsList = (props: Props) => {
+  return (
+    <>
+    <Grid container
+    direction="row"
+    justifyContent="space-around"
+    alignItems="center"
+    spacing={4}>
+        <Grid item xs={12} sm={6} md={4} >
+            <ProductsListItem></ProductsListItem>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} >
+            <ProductsListItem></ProductsListItem>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} >
+            <ProductsListItem></ProductsListItem>
+        </Grid>
+    </Grid>
+    </>
+  )
+}
+export default ProductsList
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import {Typography} from '@mui/material'
+import ProductsList from 'components/Products/ProductsList'
 
 type Props = {}
 const Home = (props: Props) => {
   return (
     <>
     <Typography variant="h4" align="center">List of Products{' '}</Typography>
+    <ProductsList></ProductsList>
     </>
   )
 }
