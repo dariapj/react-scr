6de55236e00e572c9465304ae9a0f1cdab46dc7f
daria <pojidaeva.daria@gmail.com>
2022-12-27 18:00:11 +0200

    add gret

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -6,15 +6,16 @@ import Container from '@mui/material/Container'
 import Menu from 'components/Menu/Menu';
 import CartHeader from 'components/CartHeader/CartHeader';
 import Logo from 'components/Logo/Logo'
-import classes from './Header.module.css'
+import './Header.scss'
 
 type Props = {}
 
-console.log(classes)
+
 const Header = (props: Props) => {
+  let appBar = true
   return (
     <>
-    <AppBar position="static" className={`${classes['app-bar']}`}>
+    <AppBar position="static" className={`${appBar ? 'app-bar-grey' : 'app-bar'}`}>
         <Container maxWidth="lg">
         <Toolbar>
             <IconButton
