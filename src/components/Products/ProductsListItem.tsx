9b3efe9a21380ef455ae3b041979bb18f3747278
daria <pojidaeva.daria@gmail.com>
2022-12-27 19:15:34 +0200

    add items

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -8,18 +8,18 @@ type Props = {
 capacity: string
 price: string
 }
-const ProductsListItem = (props: Props) => {
+const ProductsListItem = ({title, desc, type, capacity, price}: Props) => {
   return (
     <Card className='product' variant="outlined">
         <CardContent>
-        <div className="product-title">{props.title}</div>
-        <div className="product-dec">{props.desc}</div>
+        <div className="product-title">{title}</div>
+        <div className="product-dec">{desc}</div>
         <div className="product-features">
-            <span>Type: </span>{props.type} </div>
+            <span>Type: </span>{type} </div>
         <div className="product-features">
-            <span>Capacity:</span> {props.capacity}</div>
+            <span>Capacity:</span> {capacity}</div>
         <div className="product-price">
-        <span>Price:</span>{props.price}</div>
+        <span>Price:</span> {price}</div>
         </CardContent>
         <CardActions className="btns-wrap">
         <Button variant="outlined">Add to cart</Button>
