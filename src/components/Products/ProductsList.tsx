603366b0952c4fe89dd8fb5f1d33b1233b87e020
daria <pojidaeva.daria@gmail.com>
2022-12-28 20:09:09 +0200

    add array

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -1,4 +1,5 @@
 import {Grid} from '@mui/material'
+import productsArray from 'utils/productsArray'
 import ProductsListItem from './ProductsListItem'
 
 type Props = {}
@@ -10,15 +11,14 @@ const ProductsList = (props: Props) => {
     justifyContent="space-around"
     alignItems="center"
     spacing={4}>
-        <Grid item xs={12} sm={6} md={4} >
-            <ProductsListItem title="Iphone X" desc="This is Iphone X" type="phone" capacity="64GB" price="600$"></ProductsListItem>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} >
-            <ProductsListItem title="Iphone 14 Pro" desc="This is Iphone 14 Pro" type="tablet" capacity="120GB" price="800$"></ProductsListItem>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} >
-            <ProductsListItem title="Iphone 12" desc="This is Iphone 12" type="phone" capacity="72GB" price="400$"></ProductsListItem>
-        </Grid>
+
+        {
+            productsArray.map((product) => (
+                <Grid item xs={12} sm={6} md={4} >
+                <ProductsListItem title={product.title} desc={product.desc} type={product.type} capacity={product.capacity} price={product.price}></ProductsListItem>
+            </Grid>
+            ))
+        }
     </Grid>
     </>
   )
