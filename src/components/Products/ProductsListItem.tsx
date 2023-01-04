ce0c89004b5066338f73a42d4f3e77f998fa1620
daria <pojidaeva.daria@gmail.com>
2023-01-04 22:46:49 +0200

    add state count

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -11,24 +11,39 @@ price: string
 image: string
 }
 
-class ProductsListItem extends Component <Props>{
+type State = {
+    count:number
+}
+
+class ProductsListItem extends Component <Props, State>{
+
+constructor(props:Props) {
+    super(props)
+    this.state = {
+        count: 1,
+    }
+}
+
     render() {
+        const {
+            title, desc, type, capacity, price, image} = this.props
+
         return (
         <Card className='product' variant="outlined">
         <CardContent>
             <div className='product-img'>
-                <img src={this.props.image} alt="" />
+                <img src={image} alt="" />
             </div>
-        <div className="product-title">{this.props.title}</div>
-        <div className="product-dec">{this.props.desc}</div>
+        <div className="product-title">{title}</div>
+        <div className="product-dec">{desc}</div>
         <div className="product-features">
-            <span>Type: </span>{this.props.type} </div>
+            <span>Type: </span>{type} </div>
         <div className="product-features">
-            <span>Capacity:</span> {this.props.capacity}</div>
+            <span>Capacity:</span> {capacity}</div>
         <div className="product-price">
-        <span>Price:</span> {this.props.price}</div>
+        <span>Price:</span> {price}</div>
         <div className="product-quantity"><Button variant="outlined">-</Button>
-        <TextField value="1" size="small" /><Button variant="outlined" >+</Button></div>
+        <TextField value={this.state.count} size="small" /><Button variant="outlined" >+</Button></div>
         </CardContent>
         <CardActions className="btns-wrap">
         <Button variant="outlined">Add to cart</Button>
@@ -40,8 +55,6 @@ class ProductsListItem extends Component <Props>{
 }
 
 
-/*const ProductsListItem = ({title, desc, type, capacity, price, image}: Props) => {
 
-}*/
     
 export default ProductsListItem
